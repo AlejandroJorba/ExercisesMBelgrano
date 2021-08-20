a311c153471cb673d126440287fbfafdb1a6871f
Alejandro Jorba <alejandrojorba123@gmail.com>
2021-08-20 01:21:06 -0300

    Practica Profesionalizante

--- ejercicioAutos/autos.js
+++ ejercicioAutos/autos.js
@@ -1,19 +1,18 @@
+
 function descuentoAutos(){
- 
- descuento = document.getElementById("descuento").checked;    
- marca = document.getElementById("marca").value;
+    descuento = document.getElementById("descuento").checked;    
+    marca = document.getElementById("marca").value;
+   
+    volskwagen = document.getElementById("VW").value;
+    crysler = document.getElementById("CR").value;
+    ford = document.getElementById("FR").value;
+    ferrari = document.getElementById("FE").value;
+    seat = document.getElementById("SE").value;
+   
+    tarjeta = document.getElementById("tarjeta").checked;
+    efectivo = document.getElementById("efectivo").checked;  
+    cheque = document.getElementById("cheque").checked;
 
- volskwagen = document.getElementById("VW").value;
- crysler = document.getElementById("CR").value;
- ford = document.getElementById("FR").value;
- ferrari = document.getElementById("FE").value;
- seat = document.getElementById("SE").value;
-
- tarjeta = document.getElementById("tarjeta").checked;
- efectivo = document.getElementById("efectivo").checked;  
-
-if(descuento == true){
-    
     if (marca == volskwagen) {
         marca = volskwagen;
         valor = 40000;
@@ -36,7 +35,7 @@ if(descuento == true){
     }
     console.log(valor);
     console.log(marca);
-
+if (descuento == true){  
     if(tarjeta == true){
         recargo = valor * 0.10;
         valorFinal = valor + recargo;
@@ -44,29 +43,40 @@ if(descuento == true){
 
     }
     else if (efectivo == true){
-        descuento = valor * 0.15;
-        valorFinal = valor - descuento;
-        alert(`Pagando con efectivo tenes un descuento de ${descuento}`);
+        descuentoCalculo = valor * 0.15;
+        valorFinal = valor - descuentoCalculo;
+        alert(`Pagando con efectivo tenes un descuento de ${descuentoCalculo}`);
 
     }
-    else {
-        descuento = "No hay descuentos para pagos con cheque";
+    else if (cheque == true) {
+        descuentoMensaje = "No hay descuentos para pagos con cheque";
         valorFinal = valor;
         alert(`No hay descuentos ni recargos pagando con cheque`);
     }
+}  
+}
 
-}
-}
 
 function imprimir(){
-    if(tarjeta == true){
+    descuentoAutos();
+    if(tarjeta == true && descuento == true){
         document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio del auto es de:</b> $${valor} <br> <b>El recargo es de:</b> $${recargo} <br> <b>El precio final es de:</b> $${valorFinal} `)
     }
-    else if (efectivo == true){
-        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio del auto es de:</b> $${valor} <br> <b>El descuento es de:</b> $${descuento} <br> <b>El precio final es:</b> $${valorFinal} `)
+    else if (efectivo == true && descuento == true){
+        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio del auto es de:</b> $${valor} <br> <b>El descuento es de:</b> $${descuentoCalculo} <br> <b>El precio final es:</b> $${valorFinal} `)
 
     }
-    else {
-        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio final es:</b> $${valorFinal} <br> <b>${descuento} <b>`)
+    else if (cheque == true && descuento == true) {
+        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio final es:</b> $${valorFinal} <br> <b>${descuentoMensaje} <b>`)
     }
+    else if(tarjeta == true && descuento == false){
+        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio final es:</b> ${valor} <br> <b>No se ha seleccionado la casilla de descuento</b>`);
+    }
+    else if(efectivo == true && descuento == false){
+        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio final es:</b> ${valor} <br> <b>No se ha seleccionado la casilla de descuento</b>`);
+    }
+    else if(cheque == true && descuento == false){
+        document.write(`<b>Seleccionó la marca:</b> ${marca} <br> <b>El precio final es:</b> ${valor} <br> <b>No se ha seleccionado la casilla de descuento</b>`);
+    }
+
 }
