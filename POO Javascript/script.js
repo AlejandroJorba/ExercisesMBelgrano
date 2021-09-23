1c15770e68d4d2df975c3d506493585a9c192bcd
Alejandro Jorba <alejandrojorba123@gmail.com>
2021-09-23 19:44:08 -0300

    Practica Profesionalizante

---
+++ POO Javascript/script.js
@@ -0,0 +1,78 @@
+const h1Cancion = document.getElementById('cancion')
+const h1Autor = document.getElementById('autor')
+const btnPlay = document.getElementById('play')
+const btnPause = document.getElementById('pause')
+const btnDetener = document.getElementById('detener')
+const audio = document.getElementById('audio')
+const gif = document.getElementById('gif')
+class Cancion{
+    constructor(nombre, autor, id){
+        this.nombre = nombre;
+        this.autor = autor;
+        this.id = id;
+    }
+
+    active(x){
+        x.classList.add('bg-white');
+        return true;
+    }
+    inactive(x){
+        x.classList.remove('bg-white');
+        return true;
+    }
+    reproducir(){
+        audio.play();
+    }
+    pausar(){
+        audio.pause()
+    }
+    detener(){
+        const confirmacion = confirm('Estás seguro que desea parar la canción?')
+        if(confirmacion) {
+            audio.pause()
+            audio.currentTime = 0;
+            return true
+        }
+
+        else {
+            return false}
+    }
+
+}
+
+const cancion1 = new Cancion('Yonaguni', 'Bad Bunny', 1);
+h1Cancion.textContent = cancion1.nombre
+h1Autor.textContent = cancion1.autor
+console.log(cancion1)
+
+
+btnPlay.addEventListener('click', ()=> {
+    cancion1.reproducir()
+    if(cancion1.active(btnPause || btnDetener)){
+        cancion1.inactive(btnPause)
+        cancion1.inactive(btnDetener)
+        cancion1.active(btnPlay)
+    }
+    audio.addEventListener('ended', () => {
+        cancion1.inactive(btnPlay)
+    })
+})
+btnPause.addEventListener('click', ()=> {
+    cancion1.pausar()
+    if(cancion1.active(btnPlay || btnDetener)){
+        cancion1.inactive(btnPlay)
+        cancion1.inactive(btnDetener)
+        cancion1.active(btnPause)
+    }
+})
+btnDetener.addEventListener('click', ()=>{
+    if(cancion1.detener() == true){
+         if(cancion1.active(btnPause || btnPlay)){
+            cancion1.inactive(btnPlay)
+            cancion1.inactive(btnPause)
+            cancion1.active(btnDetener)
+        }
+    }
+    
+
+})
